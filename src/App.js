2b1f264688509c654b129c59fc02a664c117c771
maechabin <mitoppo@gmail.com>
2018-10-02 00:59:44 +0900

    HOCのサンプル実装

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <RenderPropsSample />
-        <HocSample />
+        <HocSample foo={'React'} />
         <RenderPropsHocSample />
       </div>
     );
--- src/HocSample/HocSample.js
+++ src/HocSample/HocSample.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
-class HocSample extends Component {
-  render() {
-    return <div>HocSample</div>;
-  }
+const Child = props => {
+  const { foo, bar } = props;
+  return (
+    <div>
+      <h2>child</h2>
+      <p>
+        {bar} {foo}
+      </p>
+    </div>
+  );
+};
+
+function withSample(WrappedComponent) {
+  return class extends Component {
+    constructor() {
+      super();
+      this.state = {
+        bar: 'Hello',
+      };
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>HocSample</h1>
+          <WrappedComponent bar={this.state.bar} {...this.props} />
+        </div>
+      );
+    }
+  };
 }
+
+const HocSample = withSample(Child);
 
 export default HocSample;
